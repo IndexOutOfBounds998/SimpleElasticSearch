0be7ce2e597ee7041a1f353705695025f37a597a
tokenIsme <975965279@qq.com>
2018-05-06 17:14:17 +0800

    create index

---
+++ src/main/java/pers/yang/elastichelper/clientmanager/IndexHelperBuilder.java
@@ -0,0 +1,31 @@
+package pers.yang.elastichelper.clientmanager;
+
+import org.elasticsearch.client.transport.TransportClient;
+import pers.yang.elastichelper.accessor.IndexAccessor;
+import pers.yang.elastichelper.accessor.IndexAccessorImpl;
+
+/**
+ * Created by yang on 2018/5/6.
+ * <p>
+ * 构建index mapping setting
+ */
+public class IndexHelperBuilder {
+
+    private static TransportClient client;
+
+    public static class Builder<T> {
+        private IndexHelperBuilder builder;
+
+
+        public Builder withClient(TransportClient client) {
+            IndexHelperBuilder.client = client;
+            return this;
+        }
+
+
+        public IndexAccessor creatAccessor() {
+            return new IndexAccessorImpl(client);
+        }
+
+    }
+}
